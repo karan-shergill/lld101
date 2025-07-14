fe1fbdc9e247c266ac4f654e5d6bb28103cfd68f
Amit Kumar <kumam@adobe.com>
2025-07-14 21:53:58 +0530

    practice tic tac toe lld

--- practice/LLD_TicTacToe_14july2025/src/main/java/practice/player/AI.java
+++ practice/LLD_TicTacToe_14july2025/src/main/java/practice/player/AI.java
@@ -1,10 +1,17 @@
 package practice.player;
 
 import practice.constants.PlayingSymbol;
+import practice.models.Board;
+import practice.models.Cell;
 import practice.models.Player;
 
 public class AI extends Player {
     public AI(String name, PlayingSymbol playingSymbol) {
         super(name, playingSymbol);
+    }
+
+    public Cell playMove(Board board) {
+        // scan board and whichever cell is empty return that
+        return new Cell(0, 0);
     }
 }
